b781378e42d680311eeec905afea82cfad947d7d
Vlad-Zemlyakov <vlad.zemlyakov.98@mail.ru>
2024-03-07 17:35:09 +0300

    task2

---
+++ lesson2-app, calculator/src/main/java/school/lesson1/task2.java
@@ -0,0 +1,14 @@
+package school.lesson1;
+
+public class task2 {
+    public static void main(String[] args) {
+        checkSumSign();
+    }
+    public static void checkSumSign() {
+        int a = 2;
+        int b = -3;
+        int res = a + b;
+        if (res>=0) {System.out.println("Сумма положительная");}
+        else {System.out.println("Сумма отрицательная");}
+    }
+}
