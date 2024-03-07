ecb517908d1c9bb5e5ca6a4c4deb89468f12c978
Vlad-Zemlyakov <vlad.zemlyakov.98@mail.ru>
2024-03-07 18:22:11 +0300

    task3

---
+++ lesson2-app, calculator/src/main/java/school/lesson1/task4.java
@@ -0,0 +1,14 @@
+package school.lesson1;
+
+public class task4 {
+    public static void main(String[] args) {
+        compareNumbers();
+    }
+
+    public static void compareNumbers() {
+        int a = 101;
+        int b = 102;
+        if (a>=b) {System.out.println("a>=b");}
+        else {System.out.println("a<b");}
+    }
+}
