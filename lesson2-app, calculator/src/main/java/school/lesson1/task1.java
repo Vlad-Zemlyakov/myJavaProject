740fc3ac9279c0f7a1c5ac3416a465fd7eaa7030
Vlad-Zemlyakov <vlad.zemlyakov.98@mail.ru>
2024-03-07 16:58:08 +0300

    Hello, World!

---
+++ lesson2-app, calculator/src/main/java/school/lesson1/task1.java
@@ -0,0 +1,11 @@
+package school.lesson1;
+
+public class task1 {
+    public static void main(String[] args) {
+        printThreeWords();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange \nBanana \nApple");
+    }
+}
